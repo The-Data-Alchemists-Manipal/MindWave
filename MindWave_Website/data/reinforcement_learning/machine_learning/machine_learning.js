7d1c14a6543da4a25071c0a1f408614521a4554f
Disha Modi <bhavnamodi802@gmail.com>
2023-08-08 16:08:56 +0530

    Modified the container and basic designs

---
+++ MindWave_Website/data/reinforcement_learning/machine_learning/machine_learning.js
@@ -0,0 +1,12 @@
+const reinforcement_learning = [
+    {
+        "name" : "Churn Prediction",
+        "description" : "sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum ",
+        "url_link" : "https://google.com"
+    },
+    {
+        "name" : "Churn Prediction",
+        "description" : "sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum ",
+        "url_link" : "https://google.com"
+    }
+]
